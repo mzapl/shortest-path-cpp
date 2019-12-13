9244a493573fdf151096b9844b40787358cd051f
MZAPL <maciekz16@gmail.com>
2019-12-13 10:49:56 +0100

    first working version

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <numeric> //accumulate
 #include <algorithm> //min_element, max_element
 
 using namespace std;
@@ -36,6 +37,27 @@ void printVector(vector<int> my_vect){
     cout << " }";
 }
 
+void printVector(vector<vector<int>> my_vect){
+    /* Function iterates over the values in 2D vector and prints them. */
+    cout << "{ "  << endl;
+    for (int i = 0; i < my_vect.size() ; ++i) {
+        printVector(my_vect[i]);
+    }
+    cout << endl << "}" << endl ;
+}
+
+void printVector(vector<vector<vector<int>>> my_vect){
+    /* Function iterates over the values in 3D vector and prints them. */
+    for (int i = 0; i < my_vect.size() ; ++i) {
+        for (int j = 0; j < my_vect[i].size() ; ++j) {
+            printVector(my_vect[i][j]);
+        }
+        cout << endl;
+    }
+}
+
+
+
 vector<int> getDiagonals(vector<int> diagonals, int arr_height){
     /* Function extends the vector array with possible indexes on every step/column.
      * eg. step 1: starting point 0, step 2: point 0 (going right) + point 1 (going diagonal) (cant go up, below index 0)
@@ -53,21 +75,63 @@ vector<int> getDiagonals(vector<int> diagonals, int arr_height){
     return diagonals;
 }
 
+//Rejects invalid paths where the jump is bigger than 1 index at the time (eg. 2-2-2-0-2)
+bool isValid(vector<int> path) {
+    for (int i = 0; i < path.size() - 1; ++i) {
+        if (abs(path[i] - path[i + 1]) > 1)
+            return false;
+    }
+    return true;
+}
+
+vector<vector<int>> cartesianProduct( vector<vector<int> >& v ) {
+    auto product = []( long long a, vector<int>& b ) { return a*b.size(); };
+    const long long N = accumulate( v.begin(), v.end(), 1LL, product );
+
+    vector<vector<int>> result;
+    vector<int> u(v.size());
+
+    for( long long n=0 ; n<N ; ++n ) {
+        lldiv_t q { n, 0 };
+        for( long long i=v.size()-1 ; 0<=i ; --i ) {
+            q = div( q.quot, v[i].size() );
+            u[i] = v[i][q.rem];
+        }
+
+        // Do what you want here with u.
+        if(isValid(u))
+            result.push_back(u);
+    }
+    return result ;
+}
+
+int countPoints(vector<int> path, int tab[3][5]){
+    int sum = 0, col = 0;
+    for(auto ind: path) {
+        sum += tab[ind][col];
+        col++;
+    }
+    return sum;
+}
 
 int main() {
-    //Input, declarations
     int tablica[3][5]= {
             {6, 2, 6, 1, 7},
             {4, 9, 5, 5, 8},
             {3, 1, 4, 5, 6}
             };
 
-    vector<int> diagonals;
     vector<vector<vector<int>>> indexes;
+    vector<vector<vector<int>>> paths;
+    vector<int> diagonals;
+
+    int result_val = INT_MAX;
+    vector<int> result_vect;
 
     int height = sizeof(tablica)/sizeof(tablica[0]);
     int width = sizeof(tablica[0])/sizeof(int);
 
+    //Diagonals loop
     for (int startingPoint = 0; startingPoint < height ; ++startingPoint) {
         indexes.push_back({});
         diagonals = {startingPoint};
@@ -78,16 +142,22 @@ int main() {
         }
     }
 
+    //Populate paths
+    for(auto point:indexes)
+        paths.push_back(cartesianProduct(point));
 
-    for (int i = 0; i < indexes.size() ; ++i) {
-        cout << "{";
-        for (int j = 0; j < indexes[i].size(); ++j) {
-            printVector(indexes[i][j]);
-        } cout << endl;
-    } cout << "} ";
+    //Count points for each path
+    int temp_points;
+    for(auto startingPoint: paths)
+        for(auto path: startingPoint) {
+            temp_points = countPoints(path, tablica);
+            if(temp_points < result_val) {
+                result_val = temp_points;
+                result_vect = path;
+            }
+        }
 
-
+    cout << "sum : " << result_val << endl;
+    printVector(result_vect);
     return 0;
 }
-
-
